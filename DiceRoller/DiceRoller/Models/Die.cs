68be0b6b6e8bf418bbcf201db90c079e985eb799
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-08-10 22:54:21 -0400

    Add default and parametrised constructor to set the values for dice type, sides and current side. Add method to roll the dice and get current upside of Dice

--- DiceRoller/DiceRoller/Models/Die.cs
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -6,5 +6,77 @@ namespace DiceRoller.Models
 {
     public class Die
     {
+        /**
+         * private modifier to indicate variables are not intended to be publicly
+         * accessible
+         * but should be accessible publicly via getter and setters.
+         */
+        private string diceType { get; set; }
+        private int sidesNumber { get; set; }
+        private int currentUpSide { get; set; }
+
+        // The default constructor is the no-argument constructor uninitialised fields
+        // will be set to their default values.
+        public Die()
+        {
+            diceType = "D6";
+            sidesNumber = 6;
+            rollDice(); // calling the rollDice to get value for current side up
+        }
+
+        // A parameterized constructor to initialize fields of the class with our own
+        // values. Passing number of sides as a parameter
+        public Die(int sidesVal)
+        {
+            diceType = "d" + sidesVal;
+            sidesNumber = sidesVal;
+            rollDice();
+        }
+
+        //set the value for  currentUpSide side to the  newUpSide
+        public void getCurrentUpSide(int newUpSide) 
+        {
+            if (sidesNumber >= 1 && newUpSide <= sidesNumber)
+                currentUpSide = newUpSide;
+        }
+
+        /**
+         * Method to generate Random values to get the roll for a dice
+         * and stores it in currentUpSide variable
+         */
+        public void rollDice()
+        {
+            Random r = new Random();
+            currentUpSide = r.Next(sidesNumber) + 1;
+        }
+
+       
+        /**
+         * Getter
+         * @return number of sides of dice
+         */
+        public int getSidesNumber()
+        {
+            return sidesNumber;
+        }
+
+        /**
+         * Getter
+         * @return current up Side
+         */
+        public int getCurrentUpSide()
+        {
+            return this.currentUpSide;
+        }
+
+        /**
+         * Getter
+         * @return returns the dice type
+         */
+        public string getDiceType()
+        {
+            return diceType;
+        }
+
     }
 }
