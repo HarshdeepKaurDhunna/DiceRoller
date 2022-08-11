0464e4c06c5f2de6282a10317ada7b7b3423a697
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-08-10 22:33:41 -0400

    Created a new Ms test project DieUnitTest and added refernce to DiceRoller project to excute test on Die Model Class

---
+++ DieUnitTests/DieTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DiceRoller.Models;
+namespace DieUnitTests
+{
+    [TestClass]
+    public class DieTests
+    {
+        private Die d = new Die();
+
+       
+
+
+    }
+}
