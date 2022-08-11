930f22852e53824bd690826ec045ce2bbc766960
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-08-10 23:12:10 -0400

    Add a test method to check the default values that we intialize in default constructor of Die Class

--- DieUnitTests/DieTests.cs
+++ DieUnitTests/DieTests.cs
@@ -9,13 +9,27 @@ namespace DieUnitTests
     {
         private Die d = new Die();
 
-        //By instatntiang the die class check if the class exists or not
+        /**
+         * By instatntiang the die class check if the class exists or not
+         * @TestMethod
+         */
         [TestMethod]
         public void DieNullTest()
         {
             d.Should().NotBeNull();
         }
 
-
+        /**
+        * By instatntiang the die class check that all default values are assigned or not 
+        * on the getter methods of Die class
+        * @TestMethod
+        */
+        [TestMethod]
+        public void DieHasAllDefaultValues() 
+        {
+            d.getDiceType().Should().Be("d6"); //passing parameter with "d6" as the name
+            d.getSidesNumber().Should().Be(6);//passing parameter 6 as the number of sides
+            d.getCurrentUpSide().Should().BeInRange(1, 6); // passing 2 parameter 1 and 6 as the number of sides to roll the dice 
+        }
     }
 }
